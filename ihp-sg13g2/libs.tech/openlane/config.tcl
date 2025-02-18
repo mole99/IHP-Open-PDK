c4b43e710b75401c162bddf37fa08e7275cd9b94
Leo Moser <leo.moser@efabless.com>
2025-02-18 14:00:01 +0100

    libs.tech/openlane: Update PDK and SCL config
    Signed-off-by: Leo Moser <leo.moser@efabless.com>

--- ihp-sg13g2/libs.tech/openlane/config.tcl
+++ ihp-sg13g2/libs.tech/openlane/config.tcl
@@ -7,7 +7,7 @@ if { ![info exist ::env(STD_CELL_LIBRARY)] } {
 }
 
 # Tools
-set ::env(PRIMARY_GDSII_STREAMOUT_TOOL) "klayout"
+set ::env(PRIMARY_GDSII_STREAMOUT_TOOL) "magic"
 
 # Placement site for core cells
 # This can be found in the technology lef
@@ -17,8 +17,8 @@ set ::env(GND_PIN) "VGND"
 set ::env(VDD_PIN_VOLTAGE) "1.20"
 set ::env(GND_PIN_VOLTAGE) "0.00"
 
-set ::env(STD_CELL_POWER_PINS) "VDD"
-set ::env(STD_CELL_GROUND_PINS) "VSS"
+set ::env(SCL_POWER_PINS) "VDD"
+set ::env(SCL_GROUND_PINS) "VSS"
 
 # Technology LEF
 set ::env(TECH_LEF) "$::env(PDK_ROOT)/$::env(PDK)/libs.ref/$::env(STD_CELL_LIBRARY)/lef/sg13g2_tech.lef"
@@ -43,8 +43,8 @@ set ::env(GPIO_PADS_VERILOG) "\
 #set ::env(GPIO_PADS_PREFIX) "sg13g2_IO"
 
 ## magic setup
-#set ::env(MAGIC_MAGICRC) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/magic/sg13g2.magicrc"
-#set ::env(MAGIC_TECH_FILE) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/magic/sg13g2.tech"
+set ::env(MAGICRC) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/magic/ihp-sg13g2.magicrc"
+set ::env(MAGIC_TECH) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/magic/ihp-sg13g2.tech"
 
 # Klayout setup
 set ::env(KLAYOUT_TECH) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/klayout/tech/sg13g2.lyt"
@@ -56,7 +56,7 @@ set ::env(KLAYOUT_LVS_SCRIPT) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/klayout/te
 set ::env(KLAYOUT_LVS_OPTIONS) [dict create run_mode deep ]
 
 # netgen setup
-set ::env(NETGEN_SETUP_FILE) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/netgen/sg13g2_setup.tcl"
+set ::env(NETGEN_SETUP) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/netgen/ihp-sg13g2_setup.tcl"
 
 # No tap cells
 set ::env(FP_TAPCELL_DIST) 0
@@ -108,8 +108,31 @@ set ::env(FP_PDN_CORE_RING_HOFFSET) 4.5
 set ::env(MACRO_BLOCKAGES_LAYER) "Metal1 Metal2 Metal3 Metal4 Metal5"
 
 # Used for parasitics estimation, IR drop analysis, etc
-set ::env(LAYERS_RC) "Metal1 3.49E-05 0.135e-03,Metal2 1.81E-05 0.103e-03,Metal3 2.14962E-04 0.103e-03,Metal4 1.48128E-04 0.103e-03,Metal5 1.54087E-04 0.103e-03,TopMetal1 1.54087E-04 0.021e-03,TopMetal2 1.54087E-04 0.0145e-03"
-set ::env(VIAS_RC) "Via1 2.0E-3,Via2 2.0E-3,Via3 2.0E-3,Via4 2.0E-3,TopVia1  0.4E-3,TopVia2  0.22E-3"
+set ::env(LAYERS_RC) [dict create]
+
+dict set ::env(LAYERS_RC) "*" Metal1 res 0.135e-03
+dict set ::env(LAYERS_RC) "*" Metal1 cap 3.49E-05
+dict set ::env(LAYERS_RC) "*" Metal2 res 0.103e-03
+dict set ::env(LAYERS_RC) "*" Metal2 cap 1.81E-05
+dict set ::env(LAYERS_RC) "*" Metal3 res 0.103e-03
+dict set ::env(LAYERS_RC) "*" Metal3 cap 2.14962E-04
+dict set ::env(LAYERS_RC) "*" Metal4 res 0.103e-03
+dict set ::env(LAYERS_RC) "*" Metal4 cap 1.48128E-04
+dict set ::env(LAYERS_RC) "*" Metal5 res 0.103e-03
+dict set ::env(LAYERS_RC) "*" Metal5 cap 1.54087E-04
+dict set ::env(LAYERS_RC) "*" TopMetal1 res 0.021e-03
+dict set ::env(LAYERS_RC) "*" TopMetal1 cap 1.54087E-04
+dict set ::env(LAYERS_RC) "*" TopMetal2 res 0.0145e-03
+dict set ::env(LAYERS_RC) "*" TopMetal2 cap 1.54087E-04
+
+set ::env(VIAS_R) [dict create]
+
+dict set ::env(VIAS_R) "*" Via1 res 2.0E-3
+dict set ::env(VIAS_R) "*" Via2 res 2.0E-3
+dict set ::env(VIAS_R) "*" Via3 res 2.0E-3
+dict set ::env(VIAS_R) "*" Via4 res 2.0E-3
+dict set ::env(VIAS_R) "*" TopVia1 res 0.4E-3
+dict set ::env(VIAS_R) "*" TopVia2 res 0.22E-3
 
 set ::env(DATA_WIRE_RC_LAYER) "Metal2"
 set ::env(CLOCK_WIRE_RC_LAYER) "Metal5"
--- ihp-sg13g2/libs.tech/openlane/sg13g2_stdcell/config.tcl
+++ ihp-sg13g2/libs.tech/openlane/sg13g2_stdcell/config.tcl
@@ -15,6 +15,8 @@ nom_typ_1p20V_25C \
 
 set ::env(DEFAULT_CORNER) "nom_typ_1p20V_25C"
 
+set ::env(TIMING_VIOLATION_CORNERS) "*typ*"
+
 # Synthesis mapping
  # Latch mapping
 set ::env(SYNTH_LATCH_MAP) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/openlane/$::env(STD_CELL_LIBRARY)/latch_map.v"
@@ -26,7 +28,7 @@ set ::env(SYNTH_MUX4_MAP) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/openlane/$::en
 set ::env(SYNTH_MUX_MAP) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/openlane/$::env(STD_CELL_LIBRARY)/mux2_map.v"
 
 # Tri-state buffer mapping
-set ::env(TRISTATE_BUFFER_MAP) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/openlane/$::env(STD_CELL_LIBRARY)/tribuff_map.v"
+set ::env(SYNTH_TRISTATE_MAP) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/openlane/$::env(STD_CELL_LIBRARY)/tribuff_map.v"
 
 # Placement site for core cells
 # This can be found in the technology lef
@@ -34,10 +36,11 @@ set ::env(PLACE_SITE) "CoreSite"
 set ::env(PLACE_SITE_WIDTH) 0.48
 set ::env(PLACE_SITE_HEIGHT) 3.78
 
-# welltap and endcap cells
-# FIXME: There are no Endcap and Welltie cells in this PDK, so just put dummy values
-set ::env(FP_WELLTAP_CELL) "sg13g2_fill_1"
-set ::env(FP_ENDCAP_CELL) "sg13g2_decap_4"
+# Welltap and endcap cells
+# There are no endcap and welltie cells in ihp-sg13g2
+# thus set to undefined to skip insertion
+#set ::env(WELLTAP_CELL) ""
+#set ::env(ENDCAP_CELL) ""
 
 # defaults (can be overridden by designs):
 set ::env(SYNTH_DRIVING_CELL) "sg13g2_buf_4"
@@ -47,22 +50,12 @@ set ::env(SYNTH_MIN_BUF_PORT) "sg13g2_buf_1 A X"
 set ::env(SYNTH_TIEHI_PORT) "sg13g2_tiehi L_HI"
 set ::env(SYNTH_TIELO_PORT) "sg13g2_tielo L_LO"
 
-## cts defaults
-#set ::env(CTS_ROOT_BUFFER) sky130_fd_sc_hd__clkbuf_16
-#set ::env(CELL_CLK_PORT) CLK
-
-## Placement defaults
-#set ::env(PL_LIB) $::env(LIB_TYPICAL)
-
 # Fillcell insertion
-set ::env(FILL_CELL) "sg13g2_fill_1 sg13g2_fill_2"
-set ::env(DECAP_CELL) "sg13g2_decap_*"
-#set ::env(RE_BUFFER_CELL) "sky130_fd_sc_hd__buf_4"
+set ::env(FILL_CELLS) "sg13g2_fill_1 sg13g2_fill_2"
+set ::env(DECAP_CELLS) "sg13g2_decap_*"
 
-## Diode insertaion
-#set ::env(DIODE_CELL) "sky130_fd_sc_hd__diode_2"
-#set ::env(FAKEDIODE_CELL) "sky130_ef_sc_hd__fakediode_2"
-#set ::env(DIODE_CELL_PIN) "DIODE"
+# Diode insertion
+set ::env(DIODE_CELL) "sg13g2_antennanp"
 
 set ::env(GPL_CELL_PADDING) {0}
 set ::env(DPL_CELL_PADDING) {0}
@@ -72,13 +65,11 @@ set ::env(CELL_PAD_EXCLUDE) "sg13g2_fill_* sg13g2_decap_*"
 # PDN
 set ::env(FP_PDN_RAIL_WIDTH) 0.44
 
-## Clk Buffers info CTS data
-#set ::env(ROOT_CLK_BUFFER) sky130_fd_sc_hd__clkbuf_16
-#set ::env(CTS_CLK_BUFFER_LIST) "sky130_fd_sc_hd__clkbuf_8 sky130_fd_sc_hd__clkbuf_4 sky130_fd_sc_hd__clkbuf_2"
-## Determined from https://github.com/google/skywater-pdk-libs-sky130_fd_sc_hd/blob/ac7fb61f06e6470b94e8afdf7c25268f62fbd7b1/cells/clkbuf/sky130_fd_sc_hd__clkbuf_16__tt_025C_1v80.lib.json
-#set ::env(CTS_MAX_CAP) 1.53169
-#set ::env(MAX_TRANSITION_CONSTRAINT) 0.75
-#set ::env(MAX_CAPACITANCE_CONSTRAINT) 0.2
+# CTS
+set ::env(CTS_ROOT_BUFFER) sg13g2_buf_16
+set ::env(CTS_CLK_BUFFERS) "sg13g2_buf_8 sg13g2_buf_4 sg13g2_buf_2"
+
+#set ::env(CTS_CLK_BUFFER_LIST) "sg13g2_buf_8 sg13g2_buf_4 sg13g2_buf_2"
 
 # FIXME: A bit random ... from sky130
 set ::env(MAX_FANOUT_CONSTRAINT) 10
@@ -87,5 +78,8 @@ set ::env(CLOCK_TRANSITION_CONSTRAINT) 0.15
 set ::env(TIME_DERATING_CONSTRAINT) 5
 set ::env(IO_DELAY_CONSTRAINT) 20
 
+# Tristate cells
+set ::env(TRISTATE_CELLS) "sg13g2_ebufn_* sg13g2_einvn_*"
 
-#set ::env(TRISTATE_CELL_PREFIX) "$::env(STD_CELL_LIBRARY)__ebuf"
+# TODO
+set ::env(HEURISTIC_ANTENNA_THRESHOLD) 90
